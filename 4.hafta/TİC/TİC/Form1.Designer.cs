1e7851a32896d147f0b2a73700ceb79ff7ccd9fb
yusufkrnz <wayneysf@gmail.com>
2024-11-06 12:31:35 +0300

    Yeni klasör ve dosyalar eklendi

---
+++ 4.hafta/TİC/TİC/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace TİC
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.lbl_skor = new System.Windows.Forms.Label();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.btn_restart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(110, 140);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(142, 86);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(282, 140);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(142, 86);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(444, 140);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(142, 86);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(110, 263);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(142, 86);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(282, 263);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(142, 86);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(444, 263);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(142, 86);
+            this.button6.TabIndex = 4;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(110, 378);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(142, 86);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(282, 378);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(142, 86);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(444, 378);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(142, 86);
+            this.button9.TabIndex = 6;
+            this.button9.Text = "button9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // lbl_skor
+            // 
+            this.lbl_skor.AutoSize = true;
+            this.lbl_skor.Location = new System.Drawing.Point(45, 65);
+            this.lbl_skor.Name = "lbl_skor";
+            this.lbl_skor.Size = new System.Drawing.Size(41, 17);
+            this.lbl_skor.TabIndex = 9;
+            this.lbl_skor.Text = "Skor:";
+            this.lbl_skor.Click += new System.EventHandler(this.lbl_skor_Click);
+            // 
+            // btn_start
+            // 
+            this.btn_start.Location = new System.Drawing.Point(367, 48);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(112, 51);
+            this.btn_start.TabIndex = 10;
+            this.btn_start.Text = "Başla";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // btn_restart
+            // 
+            this.btn_restart.Location = new System.Drawing.Point(495, 48);
+            this.btn_restart.Name = "btn_restart";
+            this.btn_restart.Size = new System.Drawing.Size(112, 51);
+            this.btn_restart.TabIndex = 11;
+            this.btn_restart.Text = "Tekrar";
+            this.btn_restart.UseVisualStyleBackColor = true;
+            this.btn_restart.Click += new System.EventHandler(this.btn_restart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(699, 643);
+            this.Controls.Add(this.btn_restart);
+            this.Controls.Add(this.btn_start);
+            this.Controls.Add(this.lbl_skor);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Tic Tac Toe";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Label lbl_skor;
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.Button btn_restart;
+    }
+}
+
---
+++ 4.hafta/TİC/TİC/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace TİC
+{
+    public partial class Form1 : Form
+    {
+        private bool oyuncuSirasi = true; // true -> X, false -> O
+        private int turSayisi = 0; // Tur sayısı, beraberlik kontrolü için
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Başlangıçta oyun butonlarını devre dışı bırak
+            OyunButonlariniAktifEt(false);
+        }
+
+        private void lbl_skor_Click(object sender, EventArgs e)
+        {
+            // Skor etiketi tıklama olayına özel bir işlem yok
+        }
+
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+            // Oyunu başlat
+            OyunButonlariniAktifEt(true);
+            btn_start.Enabled = false; // Başla butonunu devre dışı bırak
+            turSayisi = 0;
+            oyuncuSirasi = true; // İlk oyuncu X başlar
+        }
+
+        private void btn_restart_Click(object sender, EventArgs e)
+        {
+            // Oyunu sıfırla
+            OyunuSifirla();
+        }
+
+        // Tüm butonlar için ortak tıklama olayı
+        private void button_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+
+            // Oyuncu sırasına göre X veya O ekleyin
+            if (oyuncuSirasi)
+                button.Text = "X";
+            else
+                button.Text = "O";
+
+            button.Enabled = false; // Tıklanan butonu devre dışı bırak
+            oyuncuSirasi = !oyuncuSirasi; // Oyuncu sırasını değiştir
+            turSayisi++; // Tur sayısını artır
+
+            KazananKontrolu(); // Kazananı kontrol et
+        }
+
+        private void KazananKontrolu()
+        {
+            bool kazananVarMi = false;
+
+            // Satırları kontrol et
+            if ((button1.Text == button2.Text) && (button2.Text == button3.Text) && (!button1.Enabled))
+                kazananVarMi = true;
+            else if ((button4.Text == button5.Text) && (button5.Text == button6.Text) && (!button4.Enabled))
+                kazananVarMi = true;
+            else if ((button7.Text == button8.Text) && (button8.Text == button9.Text) && (!button7.Enabled))
+                kazananVarMi = true;
+
+            // Sütunları kontrol et
+            else if ((button1.Text == button4.Text) && (button4.Text == button7.Text) && (!button1.Enabled))
+                kazananVarMi = true;
+            else if ((button2.Text == button5.Text) && (button5.Text == button8.Text) && (!button2.Enabled))
+                kazananVarMi = true;
+            else if ((button3.Text == button6.Text) && (button6.Text == button9.Text) && (!button3.Enabled))
+                kazananVarMi = true;
+
+            // Çaprazları kontrol et
+            else if ((button1.Text == button5.Text) && (button5.Text == button9.Text) && (!button1.Enabled))
+                kazananVarMi = true;
+            else if ((button3.Text == button5.Text) && (button5.Text == button7.Text) && (!button3.Enabled))
+                kazananVarMi = true;
+
+            if (kazananVarMi)
+            {
+                OyunButonlariniPasifEt();
+
+                string kazanan = oyuncuSirasi ? "O" : "X";
+                MessageBox.Show($"{kazanan} Kazandı!");
+            }
+            else if (turSayisi == 9)
+            {
+                MessageBox.Show("Berabere!");
+            }
+        }
+
+        private void OyunButonlariniPasifEt()
+        {
+            foreach (Control kontrol in Controls)
+            {
+                if (kontrol is Button button && button != btn_restart && button != btn_start)
+                {
+                    button.Enabled = false;
+                }
+            }
+        }
+
+        private void OyunButonlariniAktifEt(bool aktifEt)
+        {
+            foreach (Control kontrol in Controls)
+            {
+                if (kontrol is Button button && button != btn_restart && button != btn_start)
+                {
+                    button.Enabled = aktifEt;
+                    if (!aktifEt)
+                        button.Text = ""; // Pasif hale getirirken buton metinlerini temizle
+                }
+            }
+        }
+
+        private void OyunuSifirla()
+        {
+            // Butonları temizle ve tekrar etkinleştir
+            OyunButonlariniAktifEt(true);
+            turSayisi = 0;
+            oyuncuSirasi = true;
+            btn_start.Enabled = true; // Başla butonunu tekrar etkinleştir
+                                      // Tüm oyun butonlarının metinlerini temizle
+            foreach (Control kontrol in Controls)
+            {
+                if (kontrol is Button button && button != btn_restart && button != btn_start)
+                {
+                    button.Text = ""; // X ve O'ları temizler
+                }
+            }
+        }
+
+        // Buton tıklama olaylarını tanımla
+        private void button1_Click(object sender, EventArgs e) => button_Click(sender, e);
+        private void button2_Click(object sender, EventArgs e) => button_Click(sender, e);
+        private void button3_Click(object sender, EventArgs e) => button_Click(sender, e);
+        private void button4_Click(object sender, EventArgs e) => button_Click(sender, e);
+        private void button5_Click(object sender, EventArgs e) => button_Click(sender, e);
+        private void button6_Click(object sender, EventArgs e) => button_Click(sender, e);
+        private void button7_Click(object sender, EventArgs e) => button_Click(sender, e);
+        private void button8_Click(object sender, EventArgs e) => button_Click(sender, e);
+        private void button9_Click(object sender, EventArgs e) => button_Click(sender, e);
+    }
+}
