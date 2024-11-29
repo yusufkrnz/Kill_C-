35d400dff3b7467f71ce92e14093ed5db5a17c17
yusufkrnz <wayneysf@gmail.com>
2024-11-29 20:40:17 +0300

    6. hafta/CarGame klasörü eklendi

---
+++ 6.hafta/CarGame/CarGame/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace CarGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer tarafından oluşturulan kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        /// içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.engel_1 = new System.Windows.Forms.PictureBox();
+            this.engel_2 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lbl_Scor = new System.Windows.Forms.Label();
+            this.btn_start = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.engel_1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.engel_2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox2.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(1718, 927);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+           // this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(50, 300);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(120, 80);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // engel_1
+            // 
+            this.engel_1.BackColor = System.Drawing.Color.Red;
+            this.engel_1.Location = new System.Drawing.Point(700, 0);
+            this.engel_1.Name = "engel_1";
+            this.engel_1.Size = new System.Drawing.Size(75, 200);
+            this.engel_1.TabIndex = 3;
+            this.engel_1.TabStop = false;
+            // 
+            // engel_2
+            // 
+            this.engel_2.BackColor = System.Drawing.Color.Red;
+            this.engel_2.Location = new System.Drawing.Point(900, -250);
+            this.engel_2.Name = "engel_2";
+            this.engel_2.Size = new System.Drawing.Size(75, 200);
+            this.engel_2.TabIndex = 4;
+            this.engel_2.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lbl_Scor
+            // 
+            this.lbl_Scor.AutoSize = true;
+            this.lbl_Scor.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Scor.Location = new System.Drawing.Point(20, 20);
+            this.lbl_Scor.Name = "lbl_Scor";
+            this.lbl_Scor.Size = new System.Drawing.Size(95, 29);
+            this.lbl_Scor.TabIndex = 5;
+            this.lbl_Scor.Text = "Skor: 0";
+            // 
+            // btn_start
+            // 
+            this.btn_start.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_start.Location = new System.Drawing.Point(400, 20);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(150, 40);
+            this.btn_start.TabIndex = 6;
+            this.btn_start.Text = "Başlat";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1363, 749);
+            this.Controls.Add(this.btn_start);
+            this.Controls.Add(this.lbl_Scor);
+            this.Controls.Add(this.engel_2);
+            this.Controls.Add(this.engel_1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Name = "Form1";
+            this.Text = "Araba Yarışı";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.engel_1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.engel_2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox engel_1;
+        private System.Windows.Forms.PictureBox engel_2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lbl_Scor;
+        private System.Windows.Forms.Button btn_start;
+    }
+}
---
+++ 6.hafta/CarGame/CarGame/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace CarGame
+{
+    public partial class Form1 : Form
+    {
+        private int arabaHiz = 10; // Arabanın hareket hızı
+        private int engelHiz = 5; // Engellerin hareket hızı
+        private bool solaHareket = false; // Sola hareket kontrolü
+        private bool sagaHareket = false; // Sağa hareket kontrolü
+        private int skor = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            OyunBaslangicAyarla();
+            this.KeyPreview = true; // Klavye olaylarını formda almak için
+        }
+
+        private void OyunBaslangicAyarla()
+        {
+            // Araba başlangıç pozisyonu
+            pictureBox1.Top = this.ClientSize.Height - pictureBox1.Height - 10;
+            pictureBox1.Left = (this.ClientSize.Width - pictureBox1.Width) / 2;
+
+            // Engeller başlangıç pozisyonları
+            engel_1.Top = -engel_1.Height;
+            engel_1.Left = this.ClientSize.Width / 3;
+
+            engel_2.Top = -engel_2.Height * 2;
+            engel_2.Left = 2 * (this.ClientSize.Width / 3);
+
+            // Skoru sıfırla
+            skor = 0;
+            lbl_Scor.Text = "Skor: " + skor;
+
+            // Timer durdur
+            timer1.Stop();
+        }
+
+        // Klavye tuşlarına basıldığında harekete geçme
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            // A tuşuna basıldığında sola hareket
+            if (e.KeyCode == Keys.A)
+                solaHareket = true;
+
+            // D tuşuna basıldığında sağa hareket
+            if (e.KeyCode == Keys.D)
+                sagaHareket = true;
+        }
+
+        // Klavye tuşlarından kaldırıldığında harekete son verme
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            // A tuşu bırakıldığında sola hareketi durdur
+            if (e.KeyCode == Keys.A)
+                solaHareket = false;
+
+            // D tuşu bırakıldığında sağa hareketi durdur
+            if (e.KeyCode == Keys.D)
+                sagaHareket = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // Araba hareketi
+            if (solaHareket && pictureBox1.Left > 0) // Sol hareket (A tuşu)
+            {
+                pictureBox1.Left -= arabaHiz; // Araba sola hareket eder
+            }
+
+            if (sagaHareket && pictureBox1.Right < this.ClientSize.Width) // Sağ hareket (D tuşu)
+            {
+                pictureBox1.Left += arabaHiz; // Araba sağa hareket eder
+            }
+
+            // Engellerin hareketi
+            engel_1.Top += engelHiz;
+            engel_2.Top += engelHiz;
+
+            // Engeller yukarıdan aşağıya doğru tekrar belirmeli
+            if (engel_1.Top > this.ClientSize.Height)
+            {
+                engel_1.Top = -engel_1.Height;
+                engel_1.Left = new Random().Next(0, this.ClientSize.Width - engel_1.Width);
+                skor++;
+            }
+
+            if (engel_2.Top > this.ClientSize.Height)
+            {
+                engel_2.Top = -engel_2.Height;
+                engel_2.Left = new Random().Next(0, this.ClientSize.Width - engel_2.Width);
+                skor++;
+            }
+
+            // Skoru güncelle
+            lbl_Scor.Text = "Skor: " + skor;
+
+            // Çarpışma kontrolü
+            if (pictureBox1.Bounds.IntersectsWith(engel_1.Bounds) || pictureBox1.Bounds.IntersectsWith(engel_2.Bounds))
+            {
+                timer1.Stop();
+                MessageBox.Show("Oyun Bitti! Skorunuz: " + skor);
+                OyunBaslangicAyarla();
+            }
+        }
+
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+            OyunBaslangicAyarla();
+            timer1.Start(); // Zamanlayıcı başlat
+        }
+    }
+}
+
+
